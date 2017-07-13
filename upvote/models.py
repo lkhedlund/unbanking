39999ade38345a0f4c109ed605dbd44ad6f9e1eb
Lars Hedlund <lkhedlund@gmail.com>
2017-07-13 14:42:44 -0700

    Added profanity filtering

--- upvote/models.py
+++ upvote/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+from upvote.data.profanity import profanity_set
 
 class Submission(models.Model):
     word = models.CharField(max_length=50, unique=True)
@@ -11,6 +14,10 @@ class Submission(models.Model):
     def save(self, *args, **kwargs):
         self.word = self.word.lower()
         super(Submission, self).save(*args, **kwargs)
+
+    def clean(self):
+        if self.word in profanity_set:
+            raise ValidationError({"word": "Let's keep this classy, please."})
 
 class Vote(models.Model):
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
