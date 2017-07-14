d83720307884802233f5a91fe301cea6a6e956a8
Lars Hedlund <lkhedlund@gmail.com>
2017-07-14 14:01:49 -0700

    Added alerts and form errors.

--- upvote/utils.py
+++ upvote/utils.py
@@ -4,7 +4,7 @@ def has_voted(request):
     voted = request.session.get('has_voted', False)
     voted_message = "Thank you for your vote. Be sure to share your word get get the most votes to win!"
     if voted:
-        messages.add_message(request, messages.ERROR, voted_message)
+        messages.add_message(request, messages.WARNING, voted_message)
         return True
     else:
         return False
